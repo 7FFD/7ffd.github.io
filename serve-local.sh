22df8fb99bfaf5477a840de34e75eb5693f2cb11
Maxim Postevoy <maxpostevoy@gmail.com>
2024-11-28 15:21:24 +0200

    added lightbox

--- serve-local.sh
+++ serve-local.sh
@@ -1,2 +1,7 @@
-bundle exec jekyll build --trace
+# After Ruby & Gems update
+# run this:
+# sudo gem install bundler
+# sudo bundle install
+# bundle exec jekyll build --trace
+
 bundle exec jekyll serve --watch
